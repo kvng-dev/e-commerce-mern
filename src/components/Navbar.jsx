dda48804f6b0c91ca91533d31a78acbfca530f3b
kvng-dev <oghuanlan@gmail.com>
2025-01-14 21:28:30 +0100

    implement add to cart

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const links = [
 ];
 const Navbar = () => {
   const [visible, setVisible] = useState(false);
-  const { search, setShowSearch, showSearch, setSearch } =
+  const { search, setShowSearch, getCartCount, showSearch, setSearch } =
     useContext(ShopContext);
   return (
     <div className="flex items-center justify-between font-medium">
@@ -68,7 +68,7 @@ const Navbar = () => {
         <Link to="/cart" className="relative">
           <img src={assets.cart_icon} alt="" className="w-5 min-w-5" />
           <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]">
-            10
+            {getCartCount()}
           </p>
         </Link>
         <img
